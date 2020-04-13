9cbbe2e3716f277f2badfc481794e4331df4e33b
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2020-04-12 17:16:04 -0700

    adding config convenience methods

--- config/config.go
+++ config/config.go
@@ -21,6 +21,10 @@ type Session struct {
 	MaxSessionDuration time.Duration
 }
 
+type UserSessionProvider interface {
+	UserSession(username string) Session
+}
+
 type Global struct {
 	Host string
 	Port uint32
@@ -28,4 +32,42 @@ type Global struct {
 	Defaults Session
 
 	UserOverrides map[string]Session
+}
+
+func (g *Global) Populate() {
+	if g.Host == "" {
+		g.Host = DefaultHost
+	}
+	if g.Port == 0 {
+		g.Port = DefaultPort
+	}
+	if g.Defaults.MaxSessionDuration == 0 {
+		g.Defaults.MaxSessionDuration = DefaultMaxSessionDuration
+	}
+	if len(g.Defaults.DefaultArgv) == 0 {
+		g.Defaults.DefaultArgv = DefaultArgv
+	}
+	if g.Defaults.Image == "" {
+		g.Defaults.Image = DefaultImage
+	}
+}
+
+func (g *Global) UserSession(username string) Session {
+	userSession, ok := g.UserOverrides[username]
+	if !ok {
+		return g.Defaults
+	}
+
+	conf := g.Defaults
+	if userSession.DefaultArgv != nil {
+		conf.DefaultArgv = userSession.DefaultArgv
+	}
+	if userSession.Image != "" {
+		conf.Image = userSession.Image
+	}
+	if userSession.MaxSessionDuration > 0 {
+		conf.MaxSessionDuration = userSession.MaxSessionDuration
+	}
+
+	return conf
 }
